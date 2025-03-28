a9ef3979f09ca8478d031163e884d92664478ea2
liujiaqi222 <2473487465@qq.com>
2025-03-28 16:42:00 +0800

    setup velite

---
+++ velite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, s } from "velite";
+
+export default defineConfig({
+  root:'content',
+  collections: {
+    posts: {
+      name: "Post",
+      pattern: "blog/**/*.md",
+      schema: s
+        .object({
+          slug: s.path(),
+          title: s.string(),
+          description: s.string().max(999).optional(),
+          date: s.isodate(),
+          published: s.boolean().default(true),
+          tags: s.array(s.string()).optional(),
+          body: s.mdx(),
+        })
+        .transform((data) => ({ ...data, slugAsParams: data.slug.split("/").slice(1).join("/") })),
+    },
+  },
+
+  output: {
+    data: '.velite',
+    assets: 'public/static',
+    base: '/static/',
+    name: '[name]-[hash:6].[ext]',
+    clean:true
+  },
+  mdx: {
+    rehypePlugins: [],
+    remarkPlugins:[]
+  }
+});
