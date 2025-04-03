33829adf37d24d27ae0ce817c986ab9a023aefc3
liujiaqi222 <2473487465@qq.com>
2025-04-03 14:17:05 +0800

    build: cloudflare worker配置

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -2,16 +2,8 @@ import { NextRequest } from "next/server";
 import { ImageResponse } from "next/og";
 import { siteConfig } from "@/config/site";
 
-export const runtime = "edge";
-
-const interBold = fetch(new URL("../../../assets/fonts/Inter-Bold.ttf", import.meta.url)).then((res) =>
-  res.arrayBuffer()
-);
-
 export async function GET(req: NextRequest) {
   try {
-    const fontBold = await interBold;
-
     const { searchParams } = req.nextUrl;
     const title = searchParams.get("title");
 
@@ -25,13 +17,7 @@ export async function GET(req: NextRequest) {
       (
         <div tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white">
           <div tw="flex items-center">
-            <svg
-              viewBox="0 0 1024 1024"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              width="50"
-              height="50"
-            >
+            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="50" height="50">
               <path
                 d="M800 192L160 424.672 567.328 832 800 192z m-107.008 107.008l-151.488 416.672-107.008-106.976 161.888-213.088-213.088 161.888-106.976-107.008 416.672-151.488z"
                 fill="#000000"
@@ -56,14 +42,6 @@ export async function GET(req: NextRequest) {
       {
         width: 1200,
         height: 630,
-        fonts: [
-          {
-            name: "Inter",
-            data: fontBold,
-            style: "normal",
-            weight: 700,
-          },
-        ],
       }
     );
   } catch {
